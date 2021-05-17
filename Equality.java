c887c65261ef56d11bd8919fc6980a4b606831b9
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-05-18 00:05:23 +0530

    Added UC-3

---
+++ Equality.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.Math;
+public class Equality
+{
+	public static void main(String arg[])
+	
+	{
+	    Scanner t=new Scanner(System.in);
+	    System.out.println("Enter the value of x1");
+	    int x1=t.nextInt();
+	    System.out.println("Enter the value of y1");
+	    int y1=t.nextInt();
+	    System.out.println("Enter the value of x2");
+	    int x2=t.nextInt();
+	    System.out.println("Enter the value of y2");
+	    int y2=t.nextInt();
+	    double length1 =((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	    System.out.println("Enter the value of X1");
+	    int X1=t.nextInt();
+	    System.out.println("Enter the value of Y1");
+	    int Y1=t.nextInt();
+	    System.out.println("Enter the value of X2");
+	    int X2=t.nextInt();
+	    System.out.println("Enter the value of Y2");
+	    int Y2=t.nextInt();
+	    double length2 =((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
+	    double s1=Math.sqrt(length1);
+	    double s2=Math.sqrt(length2);
+	    if (s1>s2){
+	        System.out.println("line 1 Is greater than line 1");
+	    } 
+	    else if (s1<s2){
+	        System.out.println("line 2 Is greater than line 1");
+	    } 
+	    else{
+	        System.out.println("line 2 Is equal to line 1");
+	    }
+	}
+ 
+}
